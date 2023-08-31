2de05a5840ca8c1c982f94e0e4c363f97608f51d
alrakib1 <abdullahalrakib30@gmail.com>
2023-08-31 19:52:44 +0600

    added category section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const handleCategory = async()=>{
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    const categories = data.data;
+
+    const tabContainer = document.getElementById('tab-container');
+    categories.forEach((categoryName)=>{
+        console.log(categoryName)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab bg-gray-300">${categoryName.category}</a> 
+        `;
+        tabContainer.appendChild(div);
+    });
+    
+}
+
+handleCategory();
