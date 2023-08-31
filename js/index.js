0d96937a0bb3874f4d435190f0a19f8193003c94
alrakib1 <abdullahalrakib30@gmail.com>
2023-08-31 22:01:10 +0600

    showing data from api complete

--- js/index.js
+++ js/index.js
@@ -5,14 +5,71 @@ const handleCategory = async()=>{
 
     const tabContainer = document.getElementById('tab-container');
     categories.forEach((categoryName)=>{
-        console.log(categoryName)
+        // console.log(categoryName)
         const div = document.createElement('div');
         div.innerHTML = `
-        <a class="tab bg-gray-300">${categoryName.category}</a> 
+        <a onclick="handleLoadVideo('${categoryName?.category_id}')" class="tab bg-gray-300">${categoryName?.category}</a> 
         `;
         tabContainer.appendChild(div);
     });
-    
+     
+}
+
+const handleLoadVideo = async(categoryId) =>{
+    console.log(categoryId)
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data =await response.json();
+    console.log(data.data);
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+
+    data.data.forEach((video)=>{
+        const div = document.createElement('div');
+
+        const blueTick = document.getElementById('blue-tick');
+        const tickMark = video.authors[0].verified;
+        // if(tickMark === false || tickMark === ''){
+        //     blueTick.classList.add = 'hidden';
+        // }else{
+        //     blueTick.classList.remove = 'hidden';
+        // }
+
+        
+        
+        div.innerHTML=`
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img src=${video.thumbnail} class="w-full h-[220px]" alt="" />
+            </figure>
+            <div class="card-body">
+              <div class="flex  gap-3">
+                <div class="avatar">
+                  <div class="w-16 h-16 rounded-full">
+                    <img src=${video.authors[0].profile_picture} alt="" />
+                  </div>
+                </div>
+                <div class="">
+                  <h4 class="text-base font-bold">
+                    ${video.title}
+                  </h4>
+                  <div>
+              <div class="flex gap-3">
+              <p>${video.authors[0].profile_name}</p>
+              <image src="./images/tick.png" class="h-1/2 " id="blue-tick"/>
+              </div>
+              <p>${video.others.views} views</p>
+              </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+        cardContainer.appendChild(div);
+
+        
+    });
 }
 
 handleCategory();
+handleLoadVideo('1000')
